433bc5355c1c0f247bd3da90790484830b44d328
Michael <michaelnjoku1200@gmail.com>
2024-07-23 11:39:41 +0200

    Fixed routing to product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -24,12 +24,8 @@ function App() {
         <Route path="/product" element={<Product/>}>
           <Route path=':productId' element={<Product/>}/>
         </Route>
-
-
         <Route path='/cart' element= {<Cart/>}/>
         <Route path='/login' element= {<LoginSignUp/>}/>
-        
-
       </Routes>
       <Footer/>
       </BrowserRouter>
